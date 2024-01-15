edc14d11c4cdb42e0dc96f9468a21a464f2577ba
leyla_qiao <775177772@qq.com>
2024-01-15 16:15:44 +0800

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,38 @@ import { TodoListContainer } from './container/TodoListContainer'
 import { ToastContainer } from './container/ToastContainer'
 import { TooltipContainer } from './container/TooltipContainer'
 import './App.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className='container'>
-      <TodoListContainer />
-      <ToastContainer />
-      <TooltipContainer />
+    <div>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todo-list" element={<TodoListContainer />} />
+          <Route path="/toast" element={<ToastContainer />} />
+          <Route path="/tooltip" element={<TooltipContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/todo-list">Todo List</Link>
+      <br />
+      <Link to="/toast">Toast</Link>
+      <br />
+      <Link to="/tooltip">Tooltip</Link>
     </div>
   )
 }
