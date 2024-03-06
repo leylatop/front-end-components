0f77d999c11a862560fb471b7654c44c9daf47af
leyla_qiao <775177772@qq.com>
2024-03-06 14:39:19 +0800

    增加AIComponent-截图调研

--- src/container/AIComponent/index.tsx
+++ src/container/AIComponent/index.tsx
@@ -1,10 +1,48 @@
+import styled from "styled-components"
+import ScreenShot from "js-web-screen-shot";
+import { useState } from "react";
 const AIComponent = () => {
+  const [imgSrc, setImgSrc] = useState('')
+  const handleScreenShot = () => {
+    const callback = ({base64, cutInfo})=>{
+      console.log(base64, cutInfo);
+      setImgSrc(base64)
+    }
+    // 截图取消时的回调函数
+    const closeFn = ()=>{
+      console.log("截图窗口关闭");
+    }
+    new ScreenShot({enableWebRtc: true, completeCallback: callback,closeCallback: closeFn});
+  }
   return (
-    <div>
-      AIComponent
-    </div>
+    <StyledAIComponent>
+      <div className="left-area">
+        <img src="/img2.png" alt="" />
+      </div>
+      <div className="right-area">
+        <button onClick={handleScreenShot}>截图</button>
+        <div>
+          <img src={imgSrc} alt="" />
+        </div>
+      </div>
+      
+    </StyledAIComponent>
   )
 }
+
+const StyledAIComponent = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 100px auto;
+
+  .left-area {
+    img {
+      width: 400px;
+    }
+  }
+`
 
 export {
   AIComponent
